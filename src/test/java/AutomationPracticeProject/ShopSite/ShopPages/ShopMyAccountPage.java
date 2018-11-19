6b637a56250e38fb4cbdd9ab5fbc4539df7de39e
Boris Zekic <bzekic@spartaglobal.com>
2018-11-19 17:02:28 +0000

    edited shopMyAccountPage with correct paths

--- src/test/java/AutomationPracticeProject/ShopSite/ShopPages/ShopMyAccountPage.java
+++ src/test/java/AutomationPracticeProject/ShopSite/ShopPages/ShopMyAccountPage.java
@@ -7,16 +7,18 @@ public class ShopMyAccountPage {
 
     private WebDriver driver;
     private String myAccountPageUrl = "http://automationpractice.com/index.php?controller=my-account";
+
     private By orderHistoryAndDetailsButtonClass = By.className("icon-list-ol");
     private By myCreditSlipsButtonClass = By.className("icon-ban-circle");
     private By myAddressesButtonClass = By.className("icon-building");
     private By myPersonalInformationButtonClass = By.className("icon-user");
     private By myWishlistsButtonClass = By.className("icon-heart");
-    private By homeButtonClass = By.className("btn btn-default button button-small");
+    private By homeButtonClass = By.xpath("//*[@id=\"center_column\"]/ul/li/a");
     private By searchProductField = By.id("search_query_top");
     private By searchProductButton = By.className("btn btn-default button-search");
-    private By checkoutButtonId = By.id("button_order_cart");
+    private By myCartButton = By.xpath("//*[@id=\"header\"]/div[3]/div/div/div[3]/div/a");
     private By signOutButtonClass = By.className("logout");
+    private By signinButtonClass = By.className("login");
 
     public ShopMyAccountPage(WebDriver driver) {
         this.driver = driver;
@@ -24,7 +26,7 @@ public class ShopMyAccountPage {
     public String getMyAccountPageUrl() {
         return myAccountPageUrl;
     }
-    public void goToMyAccountPage(){
+    public void goTomyAccountPage(){
         driver.navigate().to(myAccountPageUrl);
     }
     public void inputSearch(String product){
@@ -34,8 +36,8 @@ public class ShopMyAccountPage {
     public void clickSignOutButton(){
         driver.findElement(signOutButtonClass).click();
     }
-    public void goToCheckout(){
-        driver.findElement(checkoutButtonId).click();
+    public void goToMyCart(){
+        driver.findElement(myCartButton).click();
     }
     public void goToHome(){
         driver.findElement(homeButtonClass).click();
@@ -54,5 +56,13 @@ public class ShopMyAccountPage {
     }
     public void goToMyWishlist(){
         driver.findElement(myWishlistsButtonClass).click();
+    }
+    public boolean checkLoginStatus(){
+        if (driver.findElement(signOutButtonClass) == null){
+            return false;
+        }else if (driver.findElement(signinButtonClass) == null){
+            return true;
+        }
+        return false;
     }
 }
