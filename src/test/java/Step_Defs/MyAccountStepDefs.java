7c55b18b68c240954af2901c9c0c2de70979fe8f
Boris Zekic <bzekic@spartaglobal.com>
2018-11-19 16:59:25 +0000

    created the step defs for my account

--- src/test/java/Step_Defs/MyAccountStepDefs.java
+++ src/test/java/Step_Defs/MyAccountStepDefs.java
@@ -4,6 +4,7 @@ import AutomationPracticeProject.Selenium_Config.SeleniumDriverConfig;
 import AutomationPracticeProject.ShopSite.ShopPages.ShopSignInPage;
 import AutomationPracticeProject.ShopSite.ShopSite;
 import cucumber.api.PendingException;
+import cucumber.api.java.After;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -21,113 +22,102 @@ public class MyAccountStepDefs {
         shopSite = new ShopSite(driverConfig.getDriver());
     }
 
-
-    @Given("^That I am successfully signed in$")
-    public void that_I_am_successfully_signed_in() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        Assert.assertEquals( shopSite.shopMyAccountPage().checkLoginStatus(), true);
+    @Given("^I am on the My Account Page$")
+    public void i_am_on_the_My_Account_Page() throws Throwable {
+        shopSite.shopMyAccountPage().goTomyAccountPage();
     }
 
-    @And("^I am on the My Account Page$")
-    public void i_am_on_the_My_Account_Page() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-       shopSite.shopMyAccountPage().goTomyAccountPage();
-
+    @And("^That I am successfully signed in$")
+    public void that_I_am_successfully_signed_in() throws Throwable {
+        shopSite.shopSignInPage().inputUserName("samgrill101@gmail.com");
+        shopSite.shopSignInPage().inputPassword("engineering19");
+        shopSite.shopSignInPage().clickSubmitButton();
+        Assert.assertEquals(shopSite.getCurrentUrl(), shopSite.shopMyAccountPage().getMyAccountPageUrl());
+        //Assert.assertEquals( shopSite.shopMyAccountPage().checkLoginStatus(), true);
     }
 
     @When("^I click on the Order History and Details button$")
     public void i_click_on_the_Order_History_and_Details_button() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
        shopSite.shopMyAccountPage().goToOrderHistoryAndDetails();
     }
 
     @Then("^I go to the Order History and Details page$")
     public void i_go_to_the_Order_History_and_Details_page() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        Assert.assertEquals();
+        Assert.assertEquals("http://automationpractice.com/index.php?controller=history", shopSite.getCurrentUrl());
     }
 
     @When("^I click on the My Credit Slips button$")
     public void i_click_on_the_My_Credit_Slips_button() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        shopSite.shopMyAccountPage().goToMyCreditSlips();
     }
 
     @Then("^I go to the My Credit Slips page$")
     public void i_go_to_the_My_Credit_Slips_page() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        Assert.assertEquals("http://automationpractice.com/index.php?controller=order-slip", shopSite.getCurrentUrl());
     }
 
     @When("^I click on the My Addresses button$")
     public void i_click_on_the_My_Addresses_button() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        shopSite.shopMyAccountPage().goToMyAddresses();
     }
 
     @Then("^I go to the My Addresses page$")
     public void i_go_to_the_My_Addresses_page() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        Assert.assertEquals("http://automationpractice.com/index.php?controller=addresses", shopSite.getCurrentUrl());
     }
 
     @When("^I click on the My Personal Information button$")
     public void i_click_on_the_My_Personal_Information_button() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        shopSite.shopMyAccountPage().goToMyPersonalInformation();
     }
 
     @Then("^I go to the My Personal Information page$")
     public void i_go_to_the_My_Personal_Information_page() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        Assert.assertEquals("http://automationpractice.com/index.php?controller=identity", shopSite.getCurrentUrl());
     }
 
     @When("^I click on the My Wishlists button$")
     public void i_click_on_the_My_Wishlists_button() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        shopSite.shopMyAccountPage().goToMyWishlist();
     }
 
     @Then("^I go to the My Wishlists page$")
     public void i_go_to_the_My_Wishlists_page() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        Assert.assertEquals("http://automationpractice.com/index.php?fc=module&module=blockwishlist&controller=mywishlist", shopSite.getCurrentUrl());
     }
 
     @When("^I click on the Home button$")
     public void i_click_on_the_Home_button() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        shopSite.shopMyAccountPage().goToHome();
     }
 
     @Then("^I go to the Home page$")
     public void i_go_to_the_Home_page() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        Assert.assertEquals("http://automationpractice.com/index.php", shopSite.getCurrentUrl());
     }
 
     @When("^I click on the Sign Out button$")
     public void i_click_on_the_Sign_Out_button() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        shopSite.shopMyAccountPage().clickSignOutButton();
     }
 
     @Then("^I go to the Sign In page$")
     public void i_go_to_the_Sign_In_page() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        Assert.assertEquals("http://automationpractice.com/index.php?controller=authentication&back=my-account", shopSite.getCurrentUrl());
     }
 
-    @When("^I click on the Checkout button$")
-    public void i_click_on_the_Checkout_button() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @When("^I click on the Cart button$")
+    public void i_click_on_the_Cart_button() throws Throwable {
+        shopSite.shopMyAccountPage().goToMyCart();
     }
 
-    @Then("^I go to the Checkout page$")
-    public void i_go_to_the_Checkout_page() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Then("^I go to the Cart page$")
+    public void i_go_to_the_Cart_page() throws Throwable {
+        Assert.assertEquals("http://automationpractice.com/index.php?controller=order", shopSite.getCurrentUrl());
+    }
+
+    @After
+    public void shutDown(){
+        shopSite.quit();
     }
 }
