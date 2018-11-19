7e5adacc1c9e357547fa75230a48861626fd01ee
Sam Grill <sgrill@spartaglobal.com>
2018-11-19 10:17:58 +0000

    add constructor

--- src/test/java/AutomationPracticeProject/ShopSite/ShopPages/ShoppingCartPage.java
+++ src/test/java/AutomationPracticeProject/ShopSite/ShopPages/ShoppingCartPage.java
@@ -8,6 +8,11 @@ public class ShoppingCartPage {
 
     private WebDriver driver;
 
+    //Constructor
+    ShoppingCartPage(WebDriver driver){
+        this.driver = driver;
+    }
+
     //URLs
     private String cartUrl = "http://automationpractice.com/index.php?controller=order";
     private String checkoutAuthenticationURL = "http://automationpractice.com/index.php?controller=authentication&multi-shipping=0&display_guest_checkout=0&back=http%3A%2F%2Fautomationpractice.com%2Findex.php%3Fcontroller%3Dorder%26step%3D1%26multi-shipping%3D0";
