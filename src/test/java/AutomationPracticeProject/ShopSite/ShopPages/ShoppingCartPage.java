f0216c28289c96f626bc9ba2eb31af123f4fb291
Sam Grill <sgrill@spartaglobal.com>
2018-11-19 10:12:26 +0000

    add accesors to item prices

--- src/test/java/AutomationPracticeProject/ShopSite/ShopPages/ShoppingCartPage.java
+++ src/test/java/AutomationPracticeProject/ShopSite/ShopPages/ShoppingCartPage.java
@@ -12,7 +12,7 @@ public class ShoppingCartPage {
     private String cartUrl = "http://automationpractice.com/index.php?controller=order";
     private String checkoutAuthenticationURL = "http://automationpractice.com/index.php?controller=authentication&multi-shipping=0&display_guest_checkout=0&back=http%3A%2F%2Fautomationpractice.com%2Findex.php%3Fcontroller%3Dorder%26step%3D1%26multi-shipping%3D0";
     private String homePageURL = "http://automationpractice.com/index.php";
-    private String signInPageURL = "http://automationpractice.com/index.php?controller=authentication&back=my-account"
+    private String signInPageURL = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
 
     //Buttons
     private By proceedToCheckoutButton = By.className("button btn btn-default standard-checkout button-medium");
@@ -20,8 +20,15 @@ public class ShoppingCartPage {
     private By signInButton = By.className("login");
     private By increaseQuantityOfFirstItemButton = By.id("cart_quantity_up_1_1_0_0");
     private By decreaseQuantityOfFirstItemButton = By.id("cart_quantity_down_1_1_0_0");
+
+    //Item Quantity
     private By firstItemQuantity = By.className("quantity_2_7_0_0");
-    private By productsTotalPrice = By.id("total_product");
+    private By secondItemQuantity = By.className("quantity_3_13_0_0");
+
+    //Prices
+    private By firstItemUnitPrice = By.id("product_price_2_7_0");
+    private By secondItemUnitPrice = By.id("product_price_3_13_0");
+    private By productsTotalPrice = By.id("total_price");
 
     //Page Actions
     public void clickProceedToCheckoutButton(){
@@ -39,10 +46,26 @@ public class ShoppingCartPage {
     public void decreaseFirstItemQuantity(){
         driver.findElement(decreaseQuantityOfFirstItemButton).click();
     }
-    private Integer getFirstItemQuantity(){
+
+    //Cart data accessors
+    public Integer getFirstItemQuantity(){
         return Integer.parseInt(driver.findElement(firstItemQuantity).getAttribute("value"));
     }
-
+    public Double getTotalBasketQuantity(){
+        String totalPriceString = driver.findElement (productsTotalPrice).getAttribute ("total_price");
+        Double totalPrice = Double.parseDouble(totalPriceString.replace ("$",""));
+        return totalPrice;
+    }
+    public Double getFirstItemPrice(){
+        String firstItemPriceString = driver.findElement(firstItemUnitPrice).getAttribute ("price");
+        Double firstItemPrice = Double.parseDouble(firstItemPriceString.replace ("$",""));
+        return firstItemPrice;
+    }
+    public Double getSecondItemPrice(){
+        String seccondItemPriceString = driver.findElement(secondItemUnitPrice).getAttribute("price");
+        Double secondItemPrice = Double.parseDouble (seccondItemPriceString.replace ("$",""));
+        return secondItemPrice;
+    }
 
     //Page Accessors
     public void goToHomePage(){
@@ -54,8 +77,6 @@ public class ShoppingCartPage {
     public void goToSignInPage(){
         driver.navigate().to(signInPageURL);
     }
-
-
 
 
 }
